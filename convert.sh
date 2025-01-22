2c1dd4fb763272b9acaaeca9874c207d96a2a49d
Aleksandr Andreev <aleksandr.andreev@gmail.com>
2025-01-22 20:51:34 +0100

    Add @yanone's patch for convert.sh

--- convert.sh
+++ convert.sh
@@ -13,3 +13,9 @@ fi
 
 # Run FontForge with the provided arguments
 $FONTFORGE_BIN -lang=ff -c 'Open($1); Generate($2)' sources/Shafarik-Regular.sfd sources/Shafarik-Regular.ufo
+
+# Dirty hack to add the openTypeOS2Selection key to the fontinfo.plist file
+# because FontForge doesn't write this to the file and
+# because I'm sick of removing this from the commit staging every time
+FONTINFO=`cat sources/Shafarik-Regular.ufo/fontinfo.plist`
+echo "${FONTINFO/    <key>openTypeOS2Type<\/key>/    <key>openTypeOS2Selection</key>\n    <array>\n      <integer>7</integer>\n    </array>\n    <key>openTypeOS2Type</key>}" > sources/Shafarik-Regular.ufo/fontinfo.plist
